2b7e3c1c4576fb766c3162cfe4aba2cb4b835393
IfeAdemola <ifeoluwaademola17@gmail.com>
2024-02-17 19:51:10 +0100

    Modified inference.py

--- inference.py
+++ inference.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import joblib
 import argparse
-# from preprocessing import inference_preprocessor
+from preprocessing import inference_preprocessor
 
 def predict(model, data):
     """
@@ -37,31 +37,25 @@ def load_model(file_path):
         print(f"Error while loading the model: {str(e)}")
         return None
     
+    
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Process data")
-    parser.add_argument("--model_path", type=str, help="Path to the model file")
-    parser.add_argument("--ct_path", type=str, help="Path to the transformer file")
-    
-    parser.add_argument("--gender", type=str, help="Target column name")
-    parser.add_argument("--dystonia type", type=str, help="Target column name")
-    parser.add_argument("--amplitude", type=str, help="Target column name")
-    parser.add_argument("--voltage", type=str, help="Target column name")
-    parser.add_argument("--pulsewidth", type=str, help="Target column name")
-    parser.add_argument("--frequency", type=str, help="Target column name")
-    parser.add_argument("--position", type=str, help="Target column name")
-    parser.add_argument("--transformation_matrix", type=str, help="Target column name")
-    parser.add_argument("--leadtype", type=str, help="Target column name")
-    
-    parser.add_argument("--data_path", type=str, help="Path to the data file")
-    
+    parser.add_argument("--model_path", type=str, help="Path to the trained model file")
+    parser.add_argument("--ct_path", type=str, help="Path to the preprocessor transformer file")
+    parser.add_argument("--data_path", type=str, help="Path to the data file for inference")
+    parser.add_argument("--target", type=str, help="Specify the target variable: old totakl or new total")
+    parser.add_argument("--space", type=str, help="Specify the space: MNI or ACPC")
+    parser.add_argument('--to_delete', nargs='+', help="List of columns to be deleted")
+
     args = parser.parse_args()
     model = load_model(args.model_path)
     preprocessor = load_model(args.ct_path)
-    
+    data_path = args.data_path
+    target = float(args.target)
+    to_delete = args.to_delete
+    space = args.space
 
+    data = inference_preprocessor(data_path, target, to_delete, space, preprocessor)
 
-    model = load_model(args.model_path)
-    data = pd.read_csv(args.data_path)
-    # data = inference_preprocessor(data)
     prediction = predict(model, data)
     print(f"Prediction: {prediction}")
